4dad6eef0d429a7661e7d9ef61bcb861df9f0b32
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-09-22 17:44:56 +0300

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+const employee = {
+    name: "Alice",
+    streetAddress: "456 Elm St"
+};
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    return {
+        ...employee,  
+        [key]: value  
+    };
+};
+const updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'age', 30);
+console.log(updatedEmployee); 
+console.log(employee);
+
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value;  
+    return employee;         
+};
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, 'age', 30);
+console.log(employee);
+
+const deleteFromEmployeeByKey = (employee, key) => {
+    const { [key]: _, ...newEmployee } = employee; 
+    return newEmployee;
+}
+
+
+const updatedEmployeee = deleteFromEmployeeByKey(employee, 'age');
+console.log(updatedEmployee); 
+console.log(employee);
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key]; 
+    return employee;      
+};
+
+destructivelyDeleteFromEmployeeByKey(employee, 'age');
+console.log(employee);
